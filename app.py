e5168c8c71bd5425355a6efc4a6a477d11e9ea79
MomciloDordevic <qwertyumom@gmail.com>
2021-11-08 09:55:41 +0000

    trying to connect to database

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template, redirect, request, session, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
 
 @app.route("/")
-def hello():
-    return "Hello World ... again!"
+@app.route("/get_stat_sheet")
+def get_stat_sheet():
+    stat_sheet = mongo.db.stat_sheet.find()
+    return render_template("stat_sheet.html", stat_sheet=stat_sheet)
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
